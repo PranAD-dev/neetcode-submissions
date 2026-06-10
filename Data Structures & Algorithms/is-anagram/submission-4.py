3a9f58630f07883959650547106ea6299f3ec860
Pranjal Adhikari <pranjal.adhikari@sjsu.edu>
2026-06-09 21:37:14 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sn = [0] * 26
+        tn = [0] * 26
+
+        for char in s:
+            sn[ord(char) - ord("a")] += 1
+        for char in t:
+            tn[ord(char) - ord("a")] += 1
+        return sn == tn
