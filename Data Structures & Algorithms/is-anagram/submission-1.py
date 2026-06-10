3eef06d947d5709ef2e1538bb112d76bbc65227a
Pranjal Adhikari <pranjal.adhikari@sjsu.edu>
2026-06-09 21:36:20 -0700

    Bulk sync: 21 submissions

--- Data Structures & Algorithms/is-anagram/submission-1.py
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -1,7 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if (len(s) != len(t)):
-            return False
         sc = Counter(s)
         tc = Counter(t)
         return sc == tc
--- Data Structures & Algorithms/is-anagram/submission-3.py
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -1,7 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if (len(s) != len(t)):
-            return False
-        sc = Counter(s)
-        tc = Counter(t)
-        return sc == tc
+        sn = [0] * 26
+        tn = [0] * 26
+
+        for char in s:
+            sn[ord(char) - ord("a")] += 1
+        for char in t:
+            tn[ord(char) - ord("a")] += 1
+        return sn == tn
